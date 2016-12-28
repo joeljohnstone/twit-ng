dea3d556b9ef6ec4b8f862f6b60d5670dbc11b0a
Joel Johnstone <joel@sigalert.com>
2016-12-27 22:22:55 -0800

    Lots of changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,34 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FeedComponent } from './feed/feed.component';
+import { UserService } from './user.service';
+import { FeedService } from './feed.service';
+import { MessagesComponent } from './messages/messages.component';
+import { FriendsComponent } from './friends/friends.component';
+import { routing, appRoutingProviders } from './app.routing';
+import { FriendComponent } from './friend/friend.component';
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { MockDatabaseService } from './mock.database.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    FeedComponent
+    FeedComponent,
+    MessagesComponent,
+    FriendsComponent,
+    FriendComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing,
+    InMemoryWebApiModule.forRoot(MockDatabaseService, { 
+        delay: 100,  rootPath: 'api/'
+    }) 
   ],
-  providers: [],
+  providers: [UserService, FeedService, appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,46 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService} from "../user.service";
+import { UserService } from "../user.service";
+import { FeedService } from "../feed.service";
+import { Tweet } from "../tweet";
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.css'],
-  providers: [UserService]
+  styleUrls: ['./feed.component.css']
 })
 export class FeedComponent implements OnInit {
 
-  tweets = [
-    { body: 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.', author: 'Glen', avatar: 'glen.jpg', date: new Date(), retweets: [ 'Joe'], favorites: [] },
-    { body: 'Measuring programming progress by lines of code is like measuring aircraft building progress by weight', author: 'Joe', avatar: 'joe.jpg', date: new Date(), retweets: [], favorites: ['Mary'] },
-    { body: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', author: 'Mary', avatar: 'mary.jpg', date: new Date(), retweets: ['Glen'], favorites: ['Mary'] },
-    { body: 'People think that computer science is the art of geniuses but the actual reality is the opposite, just many people doing things that build on each other, like a wall of mini stones', author: 'Glen', avatar: 'glen.jpg', date: new Date(), retweets: [ 'Joe', 'Mary'], favorites: [] },
-    { body: 'You can’t have great software without a great team, and most software teams behave like dysfunctional families.', author: 'Joe', avatar: 'joe.jpg', date: new Date(), retweets: [], favorites: ['Mary', 'Glen'] },
-  ]
-
+  tweets = [];
   tweetText = '';
 
-  constructor(private userService : UserService) { }
+  constructor(private userService: UserService, private feedService: FeedService) { }
 
   ngOnInit() {
-  }
-
-  isUserInCollection(collection: string[], userId: string): boolean {
-    return collection.indexOf(userId) != -1;
+    console.log("FeedComponent.ngOnInit");
+    this.feedService.getCurrentFeed().subscribe((newTweets) => {
+      this.tweets = newTweets;
+    });
   }
 
   OnFavorite(tweet) {
-    if (!this.isUserInCollection(tweet.favorites, 'Glen'))
-      tweet.favorites.push('Glen');
+    this.feedService.favoriteTweet(tweet)
   }
 
   OnRetweet(tweet) {
-    if (!this.isUserInCollection(tweet.retweets, 'Glen'))
-      tweet.retweets.push('Glen');
+    this.feedService.reTweet(tweet)
   }
 
   OnNewTweet() {
     console.log(this.tweetText);
-    this.tweets.unshift({body: this.tweetText, author: 'Glen', avatar: 'glen.jpg', date: new Date(), retweets: [], favorites: []});
+    this.feedService.postNewTweet(this.tweetText).subscribe((newTweet: Tweet) => {
+      this.tweets.unshift(newTweet);
+    });
+
     this.tweetText = '';
   }
 }
