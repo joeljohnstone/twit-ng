dccf4dc1a48d8829c04e876bd62507530f5df218
Joel Johnstone <joel@sigalert.com>
2016-12-27 22:24:14 -0800

    Even more changes

---
+++ src/app/feed.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import { UserService } from './user.service';
+import { Tweet } from './tweet';
+
+@Injectable()
+export class FeedService {
+  tweets = [
+  ]
+
+  constructor(private userService: UserService, private http: Http) { }
+
+  private getTweetFromJson(obj: Tweet): Tweet {
+    return new Tweet(
+      obj.id, obj.body, obj.author, obj.date, obj.retweets, obj.favorites)
+  }
+
+  getCurrentFeed(): Observable<Tweet[]> {
+    console.log("getCurrentFeed");
+    return this.http.get('/api/tweets').map((resp: Response) => {
+      console.log(resp.json());
+      var fetchedTweets = [];
+      for (let tweet of resp.json().data) {
+        fetchedTweets.push(this.getTweetFromJson(tweet));
+      }
+      return fetchedTweets as Array<Tweet>;
+    });
+
+  }
+
+  private isUserInCollection(collection: string[], userId: string): boolean {
+    return collection.indexOf(userId) != -1;
+  }
+
+  postNewTweet(tweetText: string) {
+    let body = JSON.stringify({
+      body: tweetText, author: this.userService.getCurrentUser(),
+      date: new Date(), retweets: [], favorites: []
+    });
+
+
+    return this.http.post('/api/tweets', body).map(
+      (resp: Response) => {
+        console.log(resp.json());
+        return this.getTweetFromJson(resp.json().data);
+      });
+  }
+
+  reTweet(tweet) {
+    if (!this.isUserInCollection(tweet.retweets, this.userService.getCurrentUser())) {
+      tweet.retweets.push(this.userService.getCurrentUser());
+      this.updateTweet(tweet).subscribe(resp => console.log(resp));
+    }
+  }
+
+  favoriteTweet(tweet) {
+    if (!this.isUserInCollection(tweet.favorites, this.userService.getCurrentUser())) {
+      tweet.favorites.push(this.userService.getCurrentUser());
+      this.updateTweet(tweet).subscribe(resp => console.log(resp));
+    }
+  }
+
+  updateTweet(tweet: Tweet) {
+    let body = JSON.stringify(tweet);
+
+    let url = `/api/tweets/${tweet.id}`;
+
+    return this.http.put(url, body).map(
+      (resp: Response) => {
+        console.log(resp);
+        if (resp.status == 204) {
+          console.log("Success. Yay!");
+        }
+      });
+  }
+
+  getFriends(): Observable<string[]> {
+    return this.http.get('/assets/friends.json').map((resp: Response) => resp.json() as string[]);
+    //return ['Mary', 'Joe', 'Karen', 'Phil', 'Toni'];
+  }
+}
---
+++ src/app/tweet.ts
@@ -0,0 +1,15 @@
+export class Tweet {
+  public avatar: string;
+
+  constructor(public id: number, public body: string, public author: string, public date: Date, public retweets: Array<string>, public favorites: Array<string>) {
+    this.avatar = `${author}.jpg`;
+  }
+
+  hasFavorited(userId : string) : boolean {
+      return this.favorites.indexOf(userId) != -1;
+  }
+
+  hasRetweeted(userId : string) : boolean {
+      return this.retweets.indexOf(userId) != -1;
+  }
+}
